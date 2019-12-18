62914307a516f4818eae969df7a80c30ad93cd57
panyuhui <1137498085@qq.com>
2019-12-18 21:54:51 +0800

    第二次提交

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+登陆主键
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
