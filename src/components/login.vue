d6352414c1375604d8dbf3d4c7cfaeda6026a398
panyuhui <1137498085@qq.com>
2019-12-19 16:41:18 +0800

    我进行了第二次提交

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-登陆主键
+  <div class="login-container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+    <div class="avatar_box">
+      <img src="../assets/logo.png" alt="">
+    </div>
+    <!-- 表单区域 -->
+    <el-form label-width="0px" class="login_form" :model="formInline" :rules="rules" ref="loginForm">
+      <!-- 用户名 -->
+  <el-form-item prop="username">
+    <el-input prefix-icon="iconfont icon-user" v-model="formInline.username"></el-input>
+  </el-form-item >
+  <!-- 密码 -->
+  <el-form-item prop='password'>
+    <el-input type="password" prefix-icon="iconfont icon-3702mima" v-model='formInline.password'></el-input>
+  </el-form-item>
+  <!-- 按钮区域 -->
+  <el-form-item class="btns">
+    <el-button type="primary" @click='login'>登录</el-button>
+    <el-button @click='resetlogin'>重置</el-button>
+  </el-form-item>
+  </el-form>
+    </div>
   </div>
 </template>
 
@@ -8,16 +29,92 @@
 export default {
   data() {
     return {
+      formInline: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
     }
   },
   created(){
 
   },
   methods: {
-
+    resetlogin(){
+      console.log(this)
+      this.$refs.loginForm.resetFields()
+    },
+    login(){
+      this.$refs.loginForm.validate(valid => {
+        console.log(valid)
+        if (!valid) {
+          return false
+        }
+        this.$http.post('login', this.formInline).then((res) => {
+          console.log(res.data)
+          if (res.data.meta.status !== 200){
+            this.$message.error('登陆失败')
+          } else {
+            this.$message.success('登陆失败')
+          }
+        })
+      })
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.login-container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left:50%;
+  top:50%;
+  transform: translate(-50%,-50%);
+  .avatar_box{
+    position: absolute;
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    left: 50%;
+    transform:translate(-50%,-50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  };
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 15px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
-import ElementUI from 'element-ui'
+import ElementUI, { Message } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-
 new Vue({
   router,
   render: h => h(App)
