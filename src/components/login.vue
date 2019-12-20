263f29590c3420ed1fc5414cfd0ea8f2a0b51af2
panyuhui <1137498085@qq.com>
2019-12-20 10:19:13 +0800

    我实现了登陆退出功能

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,25 +2,25 @@
   <div class="login-container">
     <div class="login_box">
       <!-- 头像区域 -->
-    <div class="avatar_box">
-      <img src="../assets/logo.png" alt="">
-    </div>
-    <!-- 表单区域 -->
-    <el-form label-width="0px" class="login_form" :model="formInline" :rules="rules" ref="loginForm">
-      <!-- 用户名 -->
-  <el-form-item prop="username">
-    <el-input prefix-icon="iconfont icon-user" v-model="formInline.username"></el-input>
-  </el-form-item >
-  <!-- 密码 -->
-  <el-form-item prop='password'>
-    <el-input type="password" prefix-icon="iconfont icon-3702mima" v-model='formInline.password'></el-input>
-  </el-form-item>
-  <!-- 按钮区域 -->
-  <el-form-item class="btns">
-    <el-button type="primary" @click='login'>登录</el-button>
-    <el-button @click='resetlogin'>重置</el-button>
-  </el-form-item>
-  </el-form>
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 表单区域 -->
+      <el-form label-width="0px" class="login_form" :model="formInline" :rules="rules" ref="loginForm">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user" v-model="formInline.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input type="password" prefix-icon="iconfont icon-3702mima" v-model="formInline.password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button @click="resetlogin">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -45,26 +45,26 @@ export default {
       }
     }
   },
-  created(){
-
-  },
+  created() {},
   methods: {
-    resetlogin(){
+    resetlogin() {
       console.log(this)
       this.$refs.loginForm.resetFields()
     },
-    login(){
+    login() {
       this.$refs.loginForm.validate(valid => {
         console.log(valid)
         if (!valid) {
           return false
         }
-        this.$http.post('login', this.formInline).then((res) => {
+        this.$http.post('login', this.formInline).then(res => {
           console.log(res.data)
-          if (res.data.meta.status !== 200){
+          if (res.data.meta.status !== 200) {
             this.$message.error('登陆失败')
           } else {
-            this.$message.success('登陆失败')
+            this.$message.success('登陆成功')
+            window.sessionStorage.setItem('token', res.data.data.token)
+            this.$router.push('/home')
           }
         })
       })
@@ -74,7 +74,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.login-container{
+.login-container {
   background-color: #2b4b6b;
   height: 100%;
 }
@@ -84,10 +84,10 @@ export default {
   background-color: #fff;
   border-radius: 3px;
   position: absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%,-50%);
-  .avatar_box{
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
     position: absolute;
     height: 130px;
     width: 130px;
@@ -96,7 +96,7 @@ export default {
     padding: 10px;
     box-shadow: 0 0 10px #ddd;
     left: 50%;
-    transform:translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     background-color: #fff;
     img {
       width: 100%;
@@ -104,13 +104,13 @@ export default {
       border-radius: 50%;
       background-color: #eee;
     }
-  };
+  }
 }
 .btns {
   display: flex;
   justify-content: flex-end;
 }
-.login_form{
+.login_form {
   position: absolute;
   bottom: 0;
   width: 100%;
--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login.vue'
+import Home from '../components/home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+//  挂载路由守卫
+router.beforeEach((to, from, next) => {
+  //  to 将要访问的路径
+  //  from 从哪个路径跳转而来
+  //  next 函数，代表放行
+  //  next（） 放行 next（'login') 强行跳转
+  if (to.path === '/login') {
+    return next()
+  }
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
 })
 
 export default router
